01494f73acfe1c2e1f41595bd153b6d1395cacaf
Nam Anh Mai <maithenamanh@gmail.com>
2019-11-27 04:32:34 -0800

    added ListTile for options

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,22 +8,41 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   // This widget is the root of your application.
   TextStyle style = TextStyle(fontFamily: 'Open Sans');
+  final account_items = ['Account Information', 'Change Username or Password'];
+  final personal_items = ['Latest Session', 'Focus Time', 'Session History', 'Set Goals', 'Achievements'];
+  final profile_items = ['Account Information', 'Change Username or Password', 
+  'Latest Session', 'Focus Time', 'Session History', 'Set Goals', 'Achievements'];
 
+  final security_items = ['Privacy', 'Services', 'Data Storage'];
+  final eeg_items = ['Calibration'];
+  final notification_items = ['Set Reminders', 'Email Preferences'];
+  final system_items = ['Your Brake Device', 'Region', 'Language'];
+  
   @override
   Widget build(BuildContext context) {
-    final startSession = Material(
-        elevation: 5.0,
-        borderRadius: BorderRadius.circular(30.0),
-        color: Colors.blue[500],
-        child: MaterialButton(
-          minWidth: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          onPressed: () {},
-          child: Text("Login",
-              textAlign: TextAlign.center,
-              style: style.copyWith(
-                  color: Colors.white, fontWeight: FontWeight.bold)),
+     return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: new Container(
+        child: ListView.builder(
+          itemCount: profile_items.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(profile_items[index]),
+                ),
+                Divider(
+                  color: Colors.grey,
+                  thickness: 0.5,
+                ),
+              ],
+            );
+          },
         ),
-      );
+      ),
+    );
   }
 }
